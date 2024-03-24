506bcef9b7db903191f7a61835613d2eabd68c1e
Artyushenko_Daniil <foxones80@gmail.com>
2024-03-24 20:21:09 +0600

    fixed bug

--- docs/js/main.js
+++ docs/js/main.js
@@ -403,14 +403,23 @@ function clearActive() {
 const thumbs = document.querySelectorAll('.first-slide__thumb');
 const thumbsContent = document.querySelectorAll('.first-slide__content');
 thumbs.forEach(t => {
-  t.addEventListener('mouseenter', e => {
-    clearActive();
-    const dataset = t.dataset.num;
-    const content = document.querySelector(`.first-slide__content[data-slide-content="${dataset}"]`);
-    content.classList.add('active');
-    t.classList.add('active');
-  });
+  if (window.matchMedia('(min-width: 1025px)').matches) {
+    t.addEventListener('mouseenter', () => {
+      setThumbsSettings(t);
+    });
+  } else {
+    t.addEventListener('click', () => {
+      setThumbsSettings(t);
+    });
+  }
 });
+function setThumbsSettings(t) {
+  clearActive();
+  const dataset = t.dataset.num;
+  const content = document.querySelector(`.first-slide__content[data-slide-content="${dataset}"]`);
+  content.classList.add('active');
+  t.classList.add('active');
+}
 function clearActive() {
   thumbsContent.forEach(t => {
     t.classList.remove('active');
--- src/js/components/thumbs.js
+++ src/js/components/thumbs.js
@@ -2,14 +2,23 @@ const thumbs = document.querySelectorAll('.first-slide__thumb')
 const thumbsContent = document.querySelectorAll('.first-slide__content')
 
 thumbs.forEach(t => {
-  t.addEventListener('mouseenter', e => {
-    clearActive()
-    const dataset = t.dataset.num
-    const content = document.querySelector(`.first-slide__content[data-slide-content="${dataset}"]`)
-    content.classList.add('active')
-    t.classList.add('active')
-  })
+  if(window.matchMedia('(min-width: 1025px)').matches){
+    t.addEventListener('mouseenter', () => {
+      setThumbsSettings(t)
+    })
+  } else {
+    t.addEventListener('click', () => {
+      setThumbsSettings(t)
+    })
+  }
 })
+function setThumbsSettings(t){
+  clearActive()
+  const dataset = t.dataset.num
+  const content = document.querySelector(`.first-slide__content[data-slide-content="${dataset}"]`)
+  content.classList.add('active')
+  t.classList.add('active')
+}
 function clearActive() {
   thumbsContent.forEach(t => {
     t.classList.remove('active')
