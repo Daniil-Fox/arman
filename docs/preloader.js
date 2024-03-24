49dc438de51ad7ade0f0d500f9ceb396480f5958
Artyushenko_Daniil <foxones80@gmail.com>
2024-03-24 22:55:55 +0600

    renew preloader

--- docs/preloader.js
+++ docs/preloader.js
@@ -1,11 +1,28 @@
 
 window.addEventListener('DOMContentLoaded', () => {
+  var images = document.images;
+  let images_total_count = images.length
+  let images_loaded_count = 0
   const preloader = document.querySelector('.preloader')
   const video = preloader.querySelector('video')
   video.play()
-  preloader.style.opacity = '0'
 
-  setTimeout(() => {
-    preloader.remove()
-  }, 1500)
+  for(let i = 0; i < images_total_count; i++){
+    let image_clone = new Image()
+    image_clone.onload = image_loaded
+    image_clone.onerror = image_loaded
+    image_clone.src = images[i].src
+  }
+  function image_loaded(){
+    images_loaded_count++
+
+    if(images_loaded_count >= images_total_count){
+      preloader.style.opacity = '0'
+
+      setTimeout(() => {
+        preloader.remove()
+      }, 1500)
+    }
+  }
+
 })
