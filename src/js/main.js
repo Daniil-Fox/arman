8a868322a69894fd1296986efe2df7af92fa229a
Artyushenko_Daniil <foxones80@gmail.com>
2024-03-25 00:25:11 +0600

    fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,6 @@ import './_functions';
 import './_components';
 
 
-const firstSlideSlides = document.querySelectorAll('.first-slide__content')
-
-firstSlideSlides.forEach(f => {
-  const btn = f.querySelector('.first-slide__btn')
-  const cover = f.querySelector('.first-slide__cover')
-  btn.addEventListener('mouseenter', () => {
-    cover.style.width = '71rem'
-    f.classList.add('active-text')
-  })
-  btn.addEventListener('mouseleave', () => {
-    cover.style.width = null
-    f.classList.remove('active-text')
-  })
-})
-
 const secondSlide = document.querySelector('.second-slide')
 
 if(window.matchMedia('(max-width: 1024px)').matches){
--- src/resources/preloader.js
+++ src/resources/preloader.js
@@ -1,5 +1,4 @@
 
-window.addEventListener('DOMContentLoaded', () => {
   var images = document.images;
   let images_total_count = images.length
   let images_loaded_count = 0
@@ -7,22 +6,37 @@ window.addEventListener('DOMContentLoaded', () => {
   const video = preloader.querySelector('video')
   video.play()
 
+  const videoCover = document.querySelector('.first-slide__video video')
+  let flag = false
+
+
+  checkforVideo();
+
+  function checkforVideo() {
+      var b = setInterval(()=>{
+          if(videoCover.readyState>= 3){
+            flag = true
+            image_loaded()
+            clearInterval(b);
+          }
+      },500);
+  }
   for(let i = 0; i < images_total_count; i++){
     let image_clone = new Image()
     image_clone.onload = image_loaded
     image_clone.onerror = image_loaded
     image_clone.src = images[i].src
   }
+
+
   function image_loaded(){
     images_loaded_count++
-
-    if(images_loaded_count >= images_total_count){
+    if(images_loaded_count >= images_total_count && flag){
       preloader.style.opacity = '0'
 
       setTimeout(() => {
         preloader.remove()
-      }, 1500)
+      }, 4000)
     }
   }
 
-})
